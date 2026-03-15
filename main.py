6a762cfc20bd3a6248dcc4e63e33a4751e38cd0d
gtexperts23-png <gtexperts23@gmail.com>
2026-03-15 21:46:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from flask import Flask
+import threading
 import time
 
-while True:
-    print("Agent is running...")
-    time.sleep(10)
+app = Flask(__name__)
+
+def worker():
+    while True:
+        print("Agent is running...")
+        time.sleep(10)
+
+threading.Thread(target=worker).start()
+
+@app.route("/")
+def home():
+    return "Agent Alive"
